72e619f4be9d9a53e0310c622c28093bd8c0eb0b
Ruqaya91 <ruqayagilal1@gmail.com>
2025-06-11 12:55:42 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,48 +1,21 @@
-import java.sql.*;
-import java.util.Scanner;
+public class LoginExample {
+    public static void main(String[] args) throws Exception {
+        String username = "user1";
+        char[] password = "securePass123".toCharArray();
 
-public class LoginApp {
+        // Register user
+        byte[] salt = PasswordUtils.getSalt();
+        String hashedPassword = PasswordUtils.hashPassword(password, salt);
+        // Save `username` and `hashedPassword` to DB
 
-    public static void main(String[] args) {
-        // Database credentials
-        String url = "jdbc:mysql://localhost:3306/login_system";
-        String dbUser = "root";
-        String dbPass = ""; // your DB password
+        // Login
+        char[] loginPassword = "securePass123".toCharArray();
+        boolean isValid = PasswordUtils.validatePassword(loginPassword, hashedPassword);
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
-
-        System.out.print("Enter password: ");
-        String password = scanner.nextLine();
-
-        try {
-            // Load MySQL driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Connect to database
-            Connection conn = DriverManager.getConnection(url, dbUser, dbPass);
-
-            // Prepare SQL with MD5 password check
-            String sql = "SELECT * FROM users WHERE username = ? AND password = MD5(?)";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                System.out.println("Login successful. Welcome, " + username + "!");
-            } else {
-                System.out.println("Invalid username or password.");
-            }
-
-            conn.close();
-            scanner.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (isValid) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Invalid credentials.");
         }
     }
 }
